1c132c28fc34d90d764ccc8070b8bf0375d64c66
Yusuf Sohoye <yusufsohoye@Yusufs-MacBook-Air.local>
2020-06-04 20:04:14 +0100

    cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis into the disproportionate impact of Covid-19 on different groups through the prism of race. ',
+    author='Quotennial',
+    license='MIT',
+)
